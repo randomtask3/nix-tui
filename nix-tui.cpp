ed6b9bc3879d3a9cd6cfbf4cfac3bcf06114eec1
randomtask3 <nbrooker@proton.me>
2025-02-16 13:08:42 -0500

    Starting to work with ncurses, added a window and border

--- nix-tui.cpp
+++ nix-tui.cpp
@@ -1,7 +1,37 @@
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <iostream>
 #include <ncurses.h>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+//static struct winsize term_size;
+
+//static void get_terminal_size(){
+    //winsize term_size;
+//    ioctl(STDOUT_FILENO, TIOCGWINSZ, &term_size);
+//}
+//get_terminal_size();
+
+int main(int argc, char **argv) {
+    // std::cout << "Hello World \n"; // standard C++ ouput
+    
+    //get_terminal_size();
+    struct winsize term_size;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &term_size);
+
+    initscr();
+    
+    WINDOW * main_window = newwin(term_size.ws_row,term_size.ws_col-1,0,1);
+    refresh();
+
+    box(main_window,0,0);
+    
+    mvwprintw(main_window,0,1,"Label");
+    mvwprintw(main_window,1,1,"Hello World!");
+    
+    wrefresh(main_window); // Refreshes the screen to display output
+    getch();   // Waits for user input
+    endwin(); /* Closes the terminal session*/
     return 0;
 }
